060600392cd878e2c623995d6204bf73f97b6e2a
Yash-Ghatge <yashghatge012@gmail.com>
2025-08-23 17:00:33 +0530

    Update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,50 +2,55 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const { isAuthenticated } = useSelector((state) => state.user);
 
   const closeMenu = () => {
     setOpen(false);
-    document.body.classList.remove('menu-open');
+    document.body.classList.remove("menu-open");
   };
 
   const toggleMenu = () => {
-    setOpen(!open);
-    if (!open) {
-      document.body.classList.add('menu-open');
-    } else {
-      document.body.classList.remove('menu-open');
-    }
+    setOpen((prev) => !prev);
+    document.body.classList.toggle("menu-open", !open);
   };
 
-  // Clean up body class on component unmount
   useEffect(() => {
     return () => {
-      document.body.classList.remove('menu-open');
+      document.body.classList.remove("menu-open");
     };
   }, []);
 
   return (
     <nav className="navbar">
+      {/* Logo */}
       <div className="logo">
-        <img src="/j.jpg" alt="logo" />
+        <img src="/j.jpg" alt="Logo" />
       </div>
-      
+
+      {/* Navigation Menu */}
       <div className={`nav-menu ${open ? "open" : ""}`}>
         <ul>
-          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-          <li><Link to="/jobs" onClick={closeMenu}>Jobs</Link></li>
+          <li>
+            <Link to="/" onClick={closeMenu}>Home</Link>
+          </li>
+          <li>
+            <Link to="/jobs" onClick={closeMenu}>Jobs</Link>
+          </li>
           {isAuthenticated ? (
-            <li><Link to="/dashboard" onClick={closeMenu}>Dashboard</Link></li>
+            <li>
+              <Link to="/dashboard" onClick={closeMenu}>Dashboard</Link>
+            </li>
           ) : (
-            <li><Link to="/login" onClick={closeMenu}>Login</Link></li>
+            <li>
+              <Link to="/login" onClick={closeMenu}>Login</Link>
+            </li>
           )}
         </ul>
       </div>
-      
+
+      {/* Hamburger Icon */}
       <div className={`hamburger ${open ? "open" : ""}`} onClick={toggleMenu}>
         <span></span>
         <span></span>
